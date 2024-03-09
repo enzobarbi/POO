f58b5ffc766f2dde4a25fc32b8f5c2a16cf1cf46
Enzo Fagundes Barbi <enzo.fb@aluno.ifsc.edu.br>
2024-03-08 22:19:24 -0300

    Inicio POO

---
+++ Aulas/aula-07/app/src/main/java/ads/poo/Car.java
@@ -0,0 +1,25 @@
+package ads.poo;
+
+public class Car {
+
+    //Attributes (private ALWAYS)
+
+    private  double speed;
+    private String type;
+
+    //Methods
+
+    public void speedUp(double u){
+        speed+=u;
+    }
+
+    public void speedDown(double d){
+        speed-=d;
+    }
+
+    public double getSpeed(){
+        return speed;
+    }
+
+
+}
