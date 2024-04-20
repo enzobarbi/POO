56cccae71dac58da4af9d620a1fdfa8bfc47f888
Enzo Fagundes <enzo.fb@aluno.ifsc.edu.br>
2024-04-19 22:20:48 -0300

    Adicionando exercicio livro

---
+++ Exercicios/IFSC/Exercicio04/app/src/main/java/ads/poo/App.java
@@ -0,0 +1,38 @@
+package ads.poo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class App {
+    
+    public static void main(String[] args) {
+        HashMap<String, Livro> bancoDeDados =new HashMap<>();
+        
+        ArrayList<Autor> a = new ArrayList<>();
+
+        a.add(new Autor("JK Rowing", "Britsh"));
+
+        Livro livro1 = new Livro("Harry Potter", 10.00, a, "1234567891012");
+
+        a.add(new Autor("Robert Cecil Martin", "Americano"));
+
+        Livro livro2 = new Livro("Codigo Limpo", 20.0, a, "9780132350884");
+
+        if (!bancoDeDados.containsKey(livro1.getIsbn())) {
+            bancoDeDados.put(livro1.getIsbn(), livro1);
+            System.out.println("Adicionado com sucesso!");
+        }else{
+            System.out.println("Ja existe!");
+        }
+
+        if (!bancoDeDados.containsKey(livro2.getIsbn())) {
+            bancoDeDados.put(livro2.getIsbn(), livro1);
+            System.out.println("Adicionado com sucesso!");
+        }else{
+            System.out.println("Ja existe!");
+        }
+        
+        
+        System.out.println(bancoDeDados);
+    }
+}
