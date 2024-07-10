a9d430f4bef8cfba676cec420b17a8b4e2369caa
EnzoBarbi <enzo.fb@aluno.ifsc.edu.br>
2024-07-09 22:21:20 -0300

    Aula de Herança 09-07

---
+++ aulas/aulaHeranca/app/src/main/java/ads/poo/produtos/Dimensao.java
@@ -0,0 +1,21 @@
+package ads.poo.produtos;
+
+public class Dimensao {
+    private double altura;
+    private double largura;
+    private double profundidade;
+
+    public Dimensao(double altura, double largura, double profundidade) {
+        this.altura = altura;
+        this.largura = largura;
+        this.profundidade = profundidade;
+    }
+
+    public String toString() {
+        return "Dimensao{" +
+                "altura=" + altura +
+                ", largura=" + largura +
+                ", profundidade=" + profundidade +
+                '}';
+    }
+}
---
+++ aulas/aulaHeranca/app/src/main/java/ads/poo/produtos/Semfio.java
@@ -0,0 +1,19 @@
+package ads.poo.produtos;
+
+public class Semfio extends Telefone {
+    private double frequencia;
+    private int canais;
+    private double distancia;
+
+    public Semfio(int codigo, String numSerie, String modelo, double peso, Dimensao dim, double frequencia, int canais, double distancia) {
+        super(codigo, numSerie, modelo, peso, dim);
+        this.frequencia = frequencia;
+        this.canais = canais;
+        this.distancia = distancia;
+    }
+
+    @Override
+    public String imprimirDados() {
+        return super.imprimirDados() + ", frequencia= " + frequencia  + ", canais= " + canais + ", distancia= "+ distancia  ;
+    }
+}
